3c4d279012d94dd3da9514915be15daa111ac6ae
chenwenjuan <964058107@qq.com>
2018-09-09 08:02:58 +0800

    改造zipkin模块

--- zipkin/src/main/java/com/cwj/zipkin/ZipkinApplication.java
+++ zipkin/src/main/java/com/cwj/zipkin/ZipkinApplication.java
@@ -1,18 +1,29 @@
 package com.cwj.zipkin;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.EnableZipkinServer;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import zipkin.storage.mysql.MySQLStorage;
+
+
 
 /**
  * Created by cwj on 2018/9/8 23:18
  * Description:
  */
 @SpringBootApplication
-@EnableZipkinServer
+@EnableZipkinStreamServer
 public class ZipkinApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinApplication.class,args);
     }
 
+    @Bean
+    @Primary
+    public MySQLStorage mySQLStorage(DataSource datasource) {
+        return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
+    }
 }
